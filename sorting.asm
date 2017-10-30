6f5541240332b44a4c64c3891b2436e07ebd78d4
Henrik24 <henrikbastholm@gmail.com>
2017-10-30 12:18:40 +0100

    Add files via upload

--- sorting.asm
+++ sorting.asm
@@ -11,92 +11,83 @@
 
 _start:
 
-
 mov 16(%rsp), %rdx   # retrieve filename from command line argument
 
-# Open text file
+###########################################################
+################### Open text file ########################
+###########################################################
 mov $2, %rax
-mov %rdx, %rdi
-mov $0, %rsi         # Setting a flag, we use 0
-mov $0, %rdx         # 0 is equal to read-only mode
+mov %rdx, %rdi			  # Pointer to a string (filename)
+mov $0, %rsi          # Setting a flag, we use 0
+mov $0, %rdx          # 0 is equal to read-only mode
 syscall
 
-push %rax            # Put File descriptor from rax to stack
-call get_file_size   # get_file_size reads from the stack and returns in rax
+push %rax             # Put File descriptor from rax to stack
+call get_file_size    # Reads from the stack and returns filesize in rax
+pop %r12              # Put File descriptor from the stack to r12
 
+push %rax             # Put file size from rax to stack
+call alloc_mem        # Reads from the stack and returns a pointer in rax
+mov %rax, %r14        # Put the pointer to start of memory from rax to r14
+pop %r13              # Put filesize in r13
 
-pop %r12             # Put File descriptor from the stack to r12
-
-push %rax            # Put file size from rax to stack
-call alloc_mem       # alloc_mem reads from the stack and returns in rax
-
-mov %rax, %r14       # Put the Pointer to start of memory from rax to r14
-                     # r14 is the buffer size
-
-pop %r13             # Put output from get_file_size in r13 (size of file)
-
-
-#push %r13
-#call print_number
-#pop %r13
-
-
-# Copies data from the file to buffer (r14)
-# Read the n bytes from the text file and return in rax
+###########################################################
+#################### Read text file #######################
+######## Copies data from the file to buffer (r14) ########
+## Read the n bytes from the text file and return in rax ##
+###########################################################
 mov $0, %rax
-mov %r12, %rdi
-mov %r14, %rsi
-mov %r13, %rdx
+mov %r12, %rdi				# file descriptor
+mov %r14, %rsi				# pointer to memory
+mov %r13, %rdx				# num of bytes to read (filesize)
 syscall
 
-push %r13             # push filesize to stack
-push %r14             # push pointer to first buffer to stack
-call get_number_count
-pop %r14
-pop %r13
+push %r13                 # push filesize to stack
+push %r14                 # push pointer to first memory buffer to stack
+call get_number_count     # returns how many numbers is stored in buffer to rax
+pop %r14                  # pointer buffer 1
+pop %r13                  # filesize buffer 1
 
-#mov %rax, %rdx       # Save the number counted from file
+# amount of number times 8 gives us bytes needed for buffer 2
+imul $8, %rax, %r9        # filesize buffer 2
+push %r9                  # Push filesize for buffer 2 to stack
+call alloc_mem            # Returns a pointer to buffer 2 in rax
 
-push %rax
-call print_number
-pop %rax
+mov %rax, %r15            # Pointer to buffer 2 (start of memory)
 
-imul $8, %rax, %r9
-push %r9
-call print_number
-pop %r9
-
-push %r9             # Push filesize for second buffer to stack
-call alloc_mem
-pop %r9
-
-mov %rax, %r15       # Pointer to second buffer (start of memory)
-
-push %r15
-push %r9
-push %r14
+push %r15                 # Push start of buffer 2 on stack
+push %r13                 # Push filesize on stack
+push %r14                 # Push buffer 1 on stack
 call parse_number_buffer
 pop %r14
-pop %r9
+pop %r13
 pop %r15
+pop %r9
 
-
+# rcx = pointer sorted list
 # rdx = first number which needs to be moved
+# r8  = pointer to cmp value
 # r9  = filesize buffer2
-# r15 = pointer buffer2
 # r10 = minimum
-# r11 = pointer sorted list
 # r12 = cmp value
 # r13 = adress to minimum
-# r14 = counter(index)
-# r8  = pointer to cmp value
+# r14 = counter
+# r15 = pointer buffer2
 add %r15, %r9		# end of buffer
-mov %r15, %r11		# everything above r11 is sorted
+mov %r15, %rcx		# everything above r11 is sorted
+mov $0, %r14
+
+###########################################################
+################## Selection Sort #########################
+############## 1. Find minimum ############################
+############## 2. Swap with first element #################
+############## 3. Sort rest of the list ###################
+###########################################################
 
 sort:
 	# outer for loop
-	mov (%r11), %r10	# First number is minimum
-	mov %r11, %r8
+	mov (%rcx), %r10	# First number is minimum
+	mov %rcx, %r8
 
 	minimum:
 		# inner for loop
@@ -104,6 +95,7 @@ sort:
 		cmp %r8, %r9
 		je endOfList
 		mov (%r8), %r12
+		inc %r14
 		cmp %r10, %r12
 	jl newMinimum
 	jge minimum
@@ -113,16 +105,13 @@ sort:
       mov %r8, %r13
 		jmp minimum
 
-endOfList:
-  mov (%r11), %rdx    # could be done in one line, but then it crashes
+endOfList:alloc_mem r
+  mov (%rcx), %rdx    # could be done in one line, but then it crashes
   mov %rdx, (%r13)    # first number goes to minimum numbers adress
-  mov %r10, (%r11)    # minimum number goes to first numbers adress
-	add $8, %r11        # the list is sorted above r11
-  cmp %r11, %r9
+  mov %r10, (%rcx)    # minimum number goes to first numbers adress
+	add $8, %rcx        # the list is sorted above r11
+  cmp %rcx, %r9
 jne sort
-
-# så skal der bruges et print loop her
-# man kunne evt. incremente r15
 
 printing_loop:
 push (%r15)
@@ -132,27 +121,16 @@ add $8, %r15
 cmp %r15, %r9
 jne printing_loop
 
-#push 792(%r15)
-#call print_number
-#pop %rax
-
-#push (%r15)
-#call print_number
-#pop %rax
-
-
-
-
-
-
+push %r14
+call print_number
+pop %r14
 
 # Close the file again
 mov $3, %rax
 mov $3, %rdi
 syscall
 
-#Syscall to exit
-
+# Syscall to exit
 mov $60, %rax
 mov $0, %rdi
 syscall
