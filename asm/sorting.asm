50d6a327379a31ef2a27e0d5dcbf1c27133befe3
Henrik24 <henrikbastholm@gmail.com>
2017-10-30 14:51:28 +0100

    Add files via upload

--- asm/sorting.asm
+++ asm/sorting.asm
@@ -11,25 +11,25 @@
 
 _start:
 
-mov 16(%rsp), %rdx   # retrieve filename from command line argument
+mov 16(%rsp), %rdx        # Retrieve filename from command line argument
 
 ###########################################################
 ################### Open text file ########################
 ###########################################################
 mov $2, %rax
-mov %rdx, %rdi			  # Pointer to a string (filename)
-mov $0, %rsi          # Setting a flag, we use 0
-mov $0, %rdx          # 0 is equal to read-only mode
+mov %rdx, %rdi            # Pointer to a string (filename)
+mov $0, %rsi              # Setting a flag, we use 0
+mov $0, %rdx              # 0 is equal to read-only mode
 syscall
 
-push %rax             # Put File descriptor from rax to stack
-call get_file_size    # Reads from the stack and returns filesize in rax
-pop %r12              # Put File descriptor from the stack to r12
+push %rax                 # Put File descriptor from rax to stack
+call get_file_size        # Reads from the stack and returns filesize in rax
+pop %r12                  # Put File descriptor from the stack to r12
 
-push %rax             # Put file size from rax to stack
-call alloc_mem        # Reads from the stack and returns a pointer in rax
-mov %rax, %r14        # Put the pointer to start of memory from rax to r14
-pop %r13              # Put filesize in r13
+push %rax                 # Put file size from rax to stack
+call alloc_mem            # Reads from the stack and returns a pointer in rax
+mov %rax, %r14            # Put the pointer to start of memory from rax to r14
+pop %r13                  # Put filesize in r13
 
 ###########################################################
 #################### Read text file #######################
@@ -37,9 +37,9 @@ pop %r13              # Put filesize in r13
 ## Read the n bytes from the text file and return in rax ##
 ###########################################################
 mov $0, %rax
-mov %r12, %rdi				# file descriptor
-mov %r14, %rsi				# pointer to memory
-mov %r13, %rdx				# num of bytes to read (filesize)
+mov %r12, %rdi            # file descriptor
+mov %r14, %rsi            # pointer to memory
+mov %r13, %rdx            # num of bytes to read (filesize)
 syscall
 
 push %r13                 # push filesize to stack
@@ -57,25 +57,13 @@ mov %rax, %r15            # Pointer to buffer 2 (start of memory)
 
 push %r15                 # Push start of buffer 2 on stack
 push %r13                 # Push filesize on stack
-push %r14                 # Push buffer 1 on stack
-call parse_number_buffer
-pop %r14
-pop %r13
-pop %r15
-pop %r9
-
-# rcx = pointer sorted list
-# rdx = first number which needs to be moved
-# r8  = pointer to cmp value
-# r9  = filesize buffer2
-# r10 = minimum
-# r12 = cmp value
-# r13 = adress to minimum
-# r14 = counter
-# r15 = pointer buffer2
-add %r15, %r9		# end of buffer
-mov %r15, %rcx		# everything above r11 is sorted
-#mov $0, %r14
+push %r14                 # Push start of buffer 1 on stack
+call parse_number_buffer  # Writes ascii signs from buffer 1
+                          # into integers in buffer 2
+pop %r14                  # pointer buffer 1
+pop %r13                  # filesize buffer 1
+pop %r15                  # pointer buffer 2
+pop %r9                   # filesize buffer 2
 
 ###########################################################
 ################## Selection Sort #########################
@@ -83,35 +71,46 @@ mov %r15, %rcx		# everything above r11 is sorted
 ############## 2. Swap with first element #################
 ############## 3. Sort rest of the list ###################
 ###########################################################
+# rcx = pointer - above rcx list is sorted
+# r8  = pointer to cmp value
+# r9  = filesize buffer 2
+# r10 = minimum value
+# r12 = cmp value
+# r13 = pointer to minimum
+# r14 = counter
+# r15 = pointer buffer 2
+add %r15, %r9             # end of buffer
+mov %r15, %rcx            # everything above rcx is sorted
+# mov $0, %r14            # used as counter
 
-sort:
-	# outer for loop
-	mov (%rcx), %r10	# First number is minimum
-	mov %rcx, %r8
-
-	minimum:
-		# inner for loop
-		add $8, %r8
-		cmp %r8, %r9
-		je endOfList
-		mov (%r8), %r12
-		#inc %r14
-		cmp %r10, %r12
-	jl newMinimum
-	jge minimum
-
+outer:
+  # outer for loop
+  mov (%rcx), %r10        # First number is minimum
+  mov %rcx, %r8           # r8 starts by pointing at first element
+  inner:
+    # inner for loop
+    add $8, %r8           # r8 points to next number
+    cmp %r8, %r9          # have we reached the end of the buffer?
+    je endOfList          # exit inner for loop
+    mov (%r8), %r12       # r12 is temporary compare value
+    # inc %r14            # increments the counter
+    cmp %r10, %r12        # is cmp value less than minimum?
+    jl newMinimum         # if yes jump to newMinimum
+    jge inner             # if no go to next number
 		newMinimum:
-		  mov %r12, %r10
-      mov %r8, %r13
-		jmp minimum
-
+      # overwrite r10 and r13 with the new minimum value and address
+		  mov %r12, %r10      # a new minimum value is saved
+      mov %r8, %r13       # a new minimum address is saved
+		  jmp inner           # go to next number
 endOfList:
-  mov (%rcx), %rdx    # could be done in one line, but then it crashes
-  mov %rdx, (%r13)    # first number goes to minimum numbers adress
+  # The minimum of the unsorted list is found
+	# We wish to put the minimum in top of the memory (first in the list)
+  mov (%rcx), %r12    # could be done in one line, but then it crashes
+  mov %r12, (%r13)    # first number goes to minimum numbers adress
   mov %r10, (%rcx)    # minimum number goes to first numbers adress
 	add $8, %rcx        # the list is sorted above r11
   cmp %rcx, %r9
-jne sort
+  jne outer
 
 printing_loop:
 push (%r15)
@@ -121,9 +120,9 @@ add $8, %r15
 cmp %r15, %r9
 jne printing_loop
 
-#push %r14
-#call print_number
-#pop %r14
+# push %r14
+# call print_number
+# pop %r14
 
 # Close the file again
 mov $3, %rax
